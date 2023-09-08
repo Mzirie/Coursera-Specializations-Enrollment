2d28a9947f698f8dda0d97dc7b35163cd01f9761
Mahmoud Zirie <mm.zirie@gmail.com>
2023-09-08 13:14:41 +0300

    Delete Collecting directory

--- Collecting/specializations courses/Raw/pp.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_json (r"C:\Users\PC\Desktop\Projects\[Data Analytic] Coursera Spe\Collecting\RawsCourses Enroll.json")
-df.to_csv (r'C:\Users\PC\Desktop\Projects\[Data Analytic] Coursera Spe\Collecting\Raws\New_Products.csv', index = None)
